e85ea9c905bed93b3c80c97aeeb2d1b2b5e99db9
Red691 <kumarnikhil05848@gmail.com>
2025-11-16 00:24:24 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -37,28 +37,37 @@ async def decode(base64_string):
     string_bytes = base64.urlsafe_b64decode(base64_bytes)
     string = string_bytes.decode("ascii")
     return string
-
 def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
     time_list = []
     time_suffix_list = ["s", "m", "h", "days"]
+
     while count < 4:
         count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+
         if seconds == 0 and remainder == 0:
             break
+
         time_list.append(int(result))
         seconds = int(remainder)
+
     hmm = len(time_list)
+
     for x in range(hmm):
         time_list[x] = str(time_list[x]) + time_suffix_list[x]
+
     if len(time_list) == 4:
         up_time += f"{time_list.pop()}, "
+
     time_list.reverse()
     up_time += ":".join(time_list)
-    return up_time
-    
+
+    return up_time    
 async def force_sub(client, message):
     if FORCE_SUB == "False":
         return True
