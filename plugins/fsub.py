502537df455b51bd0ba05d69160d21f5d8fe2a48
Red691 <kumarnikhil05848@gmail.com>
2026-01-18 08:24:51 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,105 +1,298 @@
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery, Message, InlineKeyboardButton, InlineKeyboardMarkup
-from helper.helper_func import is_bot_admin
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+import asyncio
+import os
+import random
+import sys
+import time
+from pyrogram import Client, filters, __version__
+from pyrogram.enums import ParseMode, ChatAction, ChatMemberStatus, ChatType
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, ChatMemberUpdated, ChatPermissions
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, InviteHashEmpty, ChatAdminRequired, PeerIdInvalid, UserIsBlocked, InputUserDeactivated, UserNotParticipant
+from bot import Bot
+from config import *
+from helper_func import *
+from database.database import *
 
-#===============================================================#
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
 
-async def fsub(client, query):
-    # Create a formatted list of channels with names and IDs
-    if client.fsub_dict:
-        channel_list = []
-        for channel_id, channel_data in client.fsub_dict.items():
-            channel_name = channel_data[0] if channel_data and len(channel_data) > 0 else "Unknown"
-            request_status = "Request: ✅" if channel_data[2] else "Request: ❌"
-            timer_status = f"Timer: {channel_data[3]}m" if channel_data[3] > 0 else "Timer: ∞"
-            channel_list.append(f"• `{channel_name}` (`{channel_id}`) - {request_status}, {timer_status}")
-        
-        channels_display = "\n".join(channel_list)
-    else:
-        channels_display = "_No force subscription channels configured_"
-    
-    msg = f"""<blockquote>**Force Subscription Settings:**</blockquote>
-**Configured Channels:**
-{channels_display}
+#Request force sub mode commad,,,,,,
+@Bot.on_message(filters.command('fsub_mode') & filters.private & admin)
+async def change_force_sub_mode(client: Client, message: Message):
+    temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
+    channels = await db.show_channels()
 
-__Use the appropriate button below to add or remove a force subscription channel based on your needs!__
-"""
-    reply_markup = InlineKeyboardMarkup([
-        [InlineKeyboardButton('ᴀᴅᴅ ᴄʜᴀɴɴᴇʟ', 'add_fsub'), InlineKeyboardButton('ʀᴇᴍᴏᴠᴇ ᴄʜᴀɴɴᴇʟ', 'rm_fsub')],
-        [InlineKeyboardButton('◂ ʙᴀᴄᴋ', 'settings')]]
+    if not channels:
+        return await temp.edit("<b>❌ No force-sub channels found.</b>")
+
+    buttons = []
+    for ch_id in channels:
+        try:
+            chat = await client.get_chat(ch_id)
+            mode = await db.get_channel_mode(ch_id)
+            status = "🟢" if mode == "on" else "🔴"
+            title = f"{status} {chat.title}"
+            buttons.append([InlineKeyboardButton(title, callback_data=f"rfs_ch_{ch_id}")])
+        except:
+            buttons.append([InlineKeyboardButton(f"⚠️ {ch_id} (Unavailable)", callback_data=f"rfs_ch_{ch_id}")])
+
+    buttons.append([InlineKeyboardButton("Close ✖️", callback_data="close")])
+
+    await temp.edit(
+        "<b>⚡ Select a channel to toggle Force-Sub Mode:</b>",
+        reply_markup=InlineKeyboardMarkup(buttons),
+        disable_web_page_preview=True
     )
-    await query.message.edit_text(msg, reply_markup=reply_markup)
-    return
 
-#===============================================================#
+# This handler captures membership updates (like when a user leaves, banned)
+@Bot.on_chat_member_updated()
+async def handle_Chatmembers(client, chat_member_updated: ChatMemberUpdated):    
+    chat_id = chat_member_updated.chat.id
 
-@Client.on_callback_query(filters.regex('^add_fsub$'))
-async def add_fsub(client: Client, query: CallbackQuery):
-    await query.answer()
-    ask_channel_info = await client.ask(query.from_user.id, "Send channel id(negative integer value), request boolean(yes/no/true/false), timers(integer without decimal)(to enable it keep it greator than 0 otherwise the invite link will not have any timer to invalidate it) seperated by a space in the next 60 seconds!\n<blockquote expandable>Eg: `-10089479289 yes 5`\n\n__It means `-10089479289` is the force sub channel id, `yes` means to enable request it means the link will be request link and only after user sends request to the channel bot will work for that user even if you do not accept his request or user is not a member, `5` means timer in minutes aftetr 5 minutes the invite link will be expired.__</blockquote>", filters=filters.text, timeout=60)
+    if await db.reqChannel_exist(chat_id):
+        old_member = chat_member_updated.old_chat_member
+
+        if not old_member:
+            return
+
+        if old_member.status == ChatMemberStatus.MEMBER:
+            user_id = old_member.user.id
+
+            if await db.req_user_exist(chat_id, user_id):
+                await db.del_req_user(chat_id, user_id)
+
+
+# This handler will capture any join request to the channel/group where the bot is an admin
+@Bot.on_chat_join_request()
+async def handle_join_request(client, chat_join_request):
+    chat_id = chat_join_request.chat.id
+    user_id = chat_join_request.from_user.id
+
+    #print(f"[JOIN REQUEST] User {user_id} sent join request to {chat_id}")
+
+    # Print the result of db.reqChannel_exist to check if the channel exists
+    channel_exists = await db.reqChannel_exist(chat_id)
+    #print(f"Channel {chat_id} exists in the database: {channel_exists}")
+
+    if channel_exists:
+        if not await db.req_user_exist(chat_id, user_id):
+            await db.req_user(chat_id, user_id)
+            #print(f"Added user {user_id} to request list for {chat_id}")
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+# Add channel
+@Bot.on_message(filters.command('addchnl') & filters.private & admin)
+async def add_force_sub(client: Client, message: Message):
+    temp = await message.reply("Wait a sec...", quote=True)
+    args = message.text.split(maxsplit=1)
+
+    if len(args) != 2:
+        return await temp.edit(
+            "Usage:\n<code>/addchnl -100xxxxxxxxxx</code>"
+        )
+
     try:
-        channel_info = ask_channel_info.text.split()
-        channel_id, request, timer = channel_info
-        channel_id = int(channel_id)
-        if channel_id in client.fsub_dict.keys():
-            return await ask_channel_info.reply("**This channel id already exists in force sub list, remove it to change it's configuration!!**")
-        val, res = await is_bot_admin(client, channel_id)
-        if not val:
-            return await ask_channel_info.reply(f"**Error:** `{res}`")
-        if request.lower() in ('true', 'on', 'yes'):
-            request = True
-        elif request.lower() in ('false', 'off', 'no'):
-            request = False
-        else:
-            raise Exception("Invalid request value or type.")
-        if timer.isdigit():
-            timer = int(timer)
-        else:
-            raise Exception("Timer is not a valid integer.")
-        chat = await client.get_chat(channel_id)
-        name = chat.title
-        if timer > 0:
-            client.fsub_dict[channel_id] = [name, None, request, timer]
-        else:
-            chat_link = await client.create_chat_invite_link(channel_id, creates_join_request=request)
-            link = chat_link.invite_link
-            client.fsub_dict[channel_id] = [name, link, request, timer]
-        
-        # Update req_channels list if request is enabled
-        if request and channel_id not in client.req_channels:
-            client.req_channels.append(channel_id)
-            await client.mongodb.set_channels(client.req_channels)
-        
-        # Save to database for persistence across bot restarts
-        await client.mongodb.add_fsub_channel(channel_id, client.fsub_dict[channel_id])
-        
-        await fsub(client, query)
-        return await ask_channel_info.reply(f"__Channel with name: `{name.strip()}` is added as a force sub channel!!__")
-    except Exception as e:
-        return await ask_channel_info.reply(f"**Error:** `{e}`")
-    
-#===============================================================#
+        chat_id = int(args[1])
+    except ValueError:
+        return await temp.edit("❌ Invalid chat ID!")
 
-@Client.on_callback_query(filters.regex('^rm_fsub$'))
-async def rm_fsub(client: Client, query: CallbackQuery):
-    await query.answer()
-    ask_channel_info = await client.ask(query.from_user.id, "Send channel id(negative integer value) in the next 60 seconds!", filters=filters.text, timeout=60)
+    all_chats = await db.show_channels()
+    if chat_id in [c if isinstance(c, int) else c[0] for c in all_chats]:
+        return await temp.edit(f"Already exists:\n<code>{chat_id}</code>")
+
     try:
-        channel_id = int(ask_channel_info.text)
-        if channel_id not in client.fsub_dict.keys():
-            return await ask_channel_info.reply("**This channel id is not in force sub list!**")
-        
-        # Check if it was a request channel and remove from req_channels
-        if channel_id in client.req_channels:
-            client.req_channels.remove(channel_id)
-            await client.mongodb.set_channels(client.req_channels)
-        
-        client.fsub_dict.pop(channel_id)
-        
-        # Remove from database for persistence across bot restarts
-        await client.mongodb.remove_fsub_channel(channel_id)
-        
-        await fsub(client, query)
-        return await ask_channel_info.reply(f"__Channel with id: `{channel_id}` has been removed as a force sub channel!!__")
+        chat = await client.get_chat(chat_id)
+        if chat.type not in [ChatType.CHANNEL, ChatType.SUPERGROUP]:
+            return await temp.edit("❌ Only channels/supergroups allowed.")
+
+        bot_member = await client.get_chat_member(chat.id, "me")
+        if bot_member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
+            return await temp.edit("❌ Bot must be admin in that chat.")
+
+        # Try to get invite link
+        try:
+            link = await client.export_chat_invite_link(chat.id)
+        except Exception:
+            link = f"https://t.me/{chat.username}" if chat.username else f"https://t.me/c/{str(chat.id)[4:]}"
+
+        await db.add_channel(chat_id)
+        return await temp.edit(
+            f"✅ Added Successfully!\n\n"
+            f"<b>Name:</b> <a href='{link}'>{chat.title}</a>\n"
+            f"<b>ID:</b> <code>{chat_id}</code>",
+            disable_web_page_preview=True
+        )
+
     except Exception as e:
-        return await ask_channel_info.reply(f"**Error:** `{e}`")
+        return await temp.edit(f"❌ Failed to add chat:\n<code>{chat_id}</code>\n\n<i>{e}</i>")
+
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+# Delete channel
+@Bot.on_message(filters.command('delchnl') & filters.private & admin)
+async def del_force_sub(client: Client, message: Message):
+    temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
+    args = message.text.split(maxsplit=1)
+    all_channels = await db.show_channels()
+
+    if len(args) != 2:
+        return await temp.edit("<b>Usage:</b> <code>/delchnl <channel_id | all></code>")
+
+    if args[1].lower() == "all":
+        if not all_channels:
+            return await temp.edit("<b>❌ No force-sub channels found.</b>")
+        for ch_id in all_channels:
+            await db.del_channel(ch_id)
+        return await temp.edit("<b>✅ All force-sub channels have been removed.</b>")
+
+    try:
+        ch_id = int(args[1])
+    except ValueError:
+        return await temp.edit("<b>❌ Invalid Channel ID</b>")
+
+    if ch_id in all_channels:
+        await db.rem_channel(ch_id)
+        return await temp.edit(f"<b>✅ Channel removed:</b> <code>{ch_id}</code>")
+    else:
+        return await temp.edit(f"<b>❌ Channel not found in force-sub list:</b> <code>{ch_id}</code>")
+
+# View all channels
+@Bot.on_message(filters.command('listchnl') & filters.private & admin)
+async def list_force_sub_channels(client: Client, message: Message):
+    temp = await message.reply("<b><i>ᴡᴀɪᴛ ᴀ sᴇᴄ..</i></b>", quote=True)
+    channels = await db.show_channels()
+
+    if not channels:
+        return await temp.edit("<b>❌ No force-sub channels found.</b>")
+
+    result = "<b>⚡ Force-sub Channels:</b>\n\n"
+    for ch_id in channels:
+        try:
+            chat = await client.get_chat(ch_id)
+            link = chat.invite_link or await client.export_chat_invite_link(chat.id)
+            result += f"<b>•</b> <a href='{link}'>{chat.title}</a> [<code>{ch_id}</code>]\n"
+        except Exception:
+            result += f"<b>•</b> <code>{ch_id}</code> — <i>Unavailable</i>\n"
+
+    await temp.edit(result, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Close ✖️", callback_data="close")]]))
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+@Bot.on_message(filters.command('delreq') & filters.private & admin)
+async def delete_requested_users(client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply("⚠️ Usᴀɢᴇ: `/delreq <channel_id>`", quote=True)
+
+    try:
+        channel_id = int(message.command[1])
+    except ValueError:
+        return await message.reply("❌ Iɴᴠᴀʟɪᴅ ᴄʜᴀɴɴᴇʟ ID.", quote=True)
+
+    # Get channel request data
+    channel_data = await db.rqst_fsub_Channel_data.find_one({'_id': channel_id})
+    if not channel_data:
+        return await message.reply("ℹ️ Nᴏ ʀᴇǫᴜᴇsᴛ ᴄʜᴀɴɴᴇʟ ғᴏᴜɴᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀɴɴᴇʟ.", quote=True)
+
+    user_ids = channel_data.get("user_ids", [])
+    if not user_ids:
+        return await message.reply("✅ Nᴏ ᴜsᴇʀs ᴛᴏ ᴘʀᴏᴄᴇss.", quote=True)
+
+    removed = 0
+    skipped = 0
+    left_users = 0
+
+    for user_id in user_ids:
+        try:
+            member = await client.get_chat_member(channel_id, user_id)
+            if member.status in (
+                ChatMemberStatus.MEMBER,
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.OWNER
+            ):
+                skipped += 1  # Still a participant, and in req list
+                continue
+            else:
+                await db.del_req_user(channel_id, user_id)
+                left_users += 1
+        except UserNotParticipant:
+            await db.del_req_user(channel_id, user_id)
+            left_users += 1
+        except Exception as e:
+            print(f"[!] Error checking user {user_id}: {e}")
+            skipped += 1
+
+    for user_id in user_ids:
+        if not await db.req_user_exist(channel_id, user_id):
+            await db.del_req_user(channel_id, user_id)
+            removed += 1
+
+    return await message.reply(
+        f"✅ Cʟᴇᴀɴᴜᴘ ᴄᴏᴍᴘʟᴇᴛᴇᴅ ғᴏʀ ᴄʜᴀɴɴᴇʟ `{channel_id}`\n\n"
+        f"👤 Rᴇᴍᴏᴠᴇᴅ ᴜsᴇʀs ɴᴏᴛ ɪɴ ᴄʜᴀɴɴᴇʟ: `{left_users}`\n"
+        f"🗑️ Rᴇᴍᴏᴠᴇᴅ ʟᴇғᴛᴏᴠᴇʀ ɴᴏɴ-ʀᴇǫᴜᴇsᴛ ᴜsᴇʀs: `{removed}`\n"
+        f"✅ Sᴛɪʟʟ ᴍᴇᴍʙᴇʀs: `{skipped}`",
+        quote=True
+    )
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
